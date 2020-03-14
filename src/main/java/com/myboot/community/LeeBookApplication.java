0c560f96730ce6e5972b48622cec96af68150e3e
aibop <lijunyuo@163.com>
2020-03-14 10:23:03 +0800

    init

---
+++ src/main/java/com/myboot/community/LeeBookApplication.java
@@ -0,0 +1,13 @@
+package com.myboot.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myboot/community/LeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myboot.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
